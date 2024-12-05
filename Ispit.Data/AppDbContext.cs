cab35aeb12100d81586e0c322f713f657b47c859
Davor Hvalec <davorhvalec@gmail.com>
2024-12-05 18:24:59 +0100

    Add project files.

---
+++ Ispit.Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Ispit.Data.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ispit.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ShoppingItem> ShoppingItems { get; set; }
+}
---
+++ Ispit.Data/Entites/ShoppingItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Ispit.Data.Entites;
+
+public class ShoppingItem
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string Manufacturer { get; set; }
+}
